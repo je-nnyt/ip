012fbbcdd60d9547722477f25c0db077dcfc1c4e
je-nnyt <jennytan817@gmail.com>
2025-01-21 13:41:23 +0800

    Changed name to koya, add greetings and exit

---
+++ src/main/java/Koya.java
@@ -0,0 +1,8 @@
+public class Koya {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Koya");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! See you soon!");
+
+    }
+}
