5bfbfec78cd5b3c38a50bbd0ab84567e52faadea
Jenny Tan <jennytan817@gmail.com>
2025-01-30 16:31:22 +0800

    added the class Task and the ability to mark tasks as done by displaying a status icon

--- src/main/java/Koya.java
+++ src/main/java/Koya.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
 
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+}
+
 public class Koya {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Koya");
+
         String input;
         Scanner in = new Scanner(System.in);
-
         int countTasks = 0;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         System.out.println("What can I do for you?");
+
         do {
             input = in.nextLine();
 
@@ -19,21 +38,33 @@ public class Koya {
 
             //display list
             if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < countTasks; i++) {
-                    System.out.println(i + 1 + ". " + list[i]);
+                    System.out.println((i + 1) + ". " + list[i].getStatusIcon() + " " + list[i].description);
                 }
+                continue;
+            }
+
+            //mark as done
+            if (input.startsWith("mark")) {
+                int spaceIndex = input.indexOf(" ");
+                int taskIndex = Integer.parseInt(input.substring(spaceIndex + 1)) - 1;
+                list[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this as done:");
+                System.out.println(list[taskIndex].getStatusIcon() + " " + list[taskIndex].description);
+                continue;
             }
 
             //store task into list
             else {
-                list[countTasks] = input;
-                System.out.println("added: " + list[countTasks]);
+                list[countTasks] = new Task(input);
+                System.out.println("added: " + list[countTasks].description);
                 countTasks++;
             }
 
         } while (true);
 
         System.out.println("Bye Bye! See you soon!");
-
     }
 }
+
