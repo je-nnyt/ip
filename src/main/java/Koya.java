b235581bf8922cea9a285ac424f646d261d34f4b
Jenny Tan <jennytan817@gmail.com>
2025-01-30 14:57:15 +0800

    add ability to store information entered by the user and to list them back in order when requested

--- src/main/java/Koya.java
+++ src/main/java/Koya.java
@@ -3,16 +3,37 @@ import java.util.Scanner;
 public class Koya {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Koya");
-        String line;
+        String input;
         Scanner in = new Scanner(System.in);
 
+        int countTasks = 0;
+        String[] list = new String[100];
         System.out.println("What can I do for you?");
         do {
-            line = in.nextLine();
-            System.out.println(line);
-        } while (!line.equals("bye"));
+            input = in.nextLine();
 
-        System.out.println("Bye! See you soon!");
+            //exit
+            if (input.equals("bye")) {
+                break;
+            }
+
+            //display list
+            if (input.equals("list")) {
+                for (int i = 0; i < countTasks; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
+            }
+
+            //store task into list
+            else {
+                list[countTasks] = input;
+                System.out.println("added: " + list[countTasks]);
+                countTasks++;
+            }
+
+        } while (true);
+
+        System.out.println("Bye Bye! See you soon!");
 
     }
 }
