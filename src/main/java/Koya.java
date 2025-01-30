8e24459ee9bb01f41086628cde5925b810773d37
Jenny Tan <jennytan817@gmail.com>
2025-01-30 14:14:53 +0800

    echos commands entered by user
    and exits when user enters "bye" in the command

--- src/main/java/Koya.java
+++ src/main/java/Koya.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Koya {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Koya");
+        String line;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("What can I do for you?");
+        do {
+            line = in.nextLine();
+            System.out.println(line);
+        } while (!line.equals("bye"));
+
         System.out.println("Bye! See you soon!");
 
     }
