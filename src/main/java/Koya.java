5da7e4b3fa8ead4987871394fc12744b8deae124
Jenny Tan <jennytan817@gmail.com>
2025-02-05 18:51:37 +0800

    Extracted methods: confirmMarkDone, listTasks, confirmAddTask Set taskCount as a global variable to fix taskCount not incrementing after the task has been added to list Add comments, reformatting code

--- src/main/java/Koya.java
+++ src/main/java/Koya.java
@@ -14,7 +14,7 @@ class Task {
     }
 
     public String getTaskTypeIcon() {
-        return " ";
+        return " ";//leave empty, overridden by subclass
     }
 
     public void markAsDone() {
@@ -26,7 +26,7 @@ class Task {
     }
 }
 
-class Todo extends Task { //DONE
+class Todo extends Task {
 
     public Todo(String description) {
         super(description);
@@ -62,7 +62,7 @@ class Deadline extends Task {
         return super.toString() + " (by: " + by + ")";
     }
 
-} // input: description /deadline
+}
 
 
 class Event extends Task {
@@ -90,83 +90,89 @@ class Event extends Task {
 
 
 public class Koya {
+
+    private static int taskCount = 0;
+    private static final int MAX_TASKS = 100;
+    private static Task[] list = new Task[MAX_TASKS];
+
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm Koya");
-
-        int taskCount = 0;
-        int MAX_TASKS = 100;
-        Task[] list = new Task[MAX_TASKS];
-
         String input;
+
         Scanner in = new Scanner(System.in);
         System.out.println("What can I do for you?");
 
-        do {
+        while (true) {
             input = in.nextLine();
 
             if (input.equals("bye")) {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + " " + list[i].toString());
-                }
+                listTasks(list);
 
             } else if (input.startsWith("mark")) {
-                //obtain task number to mark as done
+                //indexes to obtain number next to mark
                 int spaceIndex = input.indexOf(" ");
                 int taskIndex = Integer.parseInt(input.substring(spaceIndex + 1)) - 1;
+                //-1 to adjust for zero-based index array (e.g. mark 2 should be at index 1 in the array)
 
                 list[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this as done:");
-                System.out.println(list[taskIndex].toString());
+                confirmMarkDone(list[taskIndex]);
 
             } else if (input.startsWith("todo")) {
                 String description = input.substring(5);
-                list[taskCount] = new Todo(description); // create todo object and add to list
-                System.out.println("Got it. I've added this task:");
-                System.out.println(list[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have "+ taskCount +" tasks in the list.");
 
-            } else if (input.startsWith("deadline")){
-                //parse to obtain description and by
-                int dividerPosition= input.indexOf(" /by ");
-                String description = input.substring(9,dividerPosition);
-                String by = input.substring(dividerPosition+5);
-                list[taskCount] = new Deadline(description,by);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(list[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have "+ taskCount +" tasks in the list.");
+                list[taskCount] = new Todo(description);
+                confirmAddTask(list);
 
-            } else if (input.startsWith("event")){
-                //parsing index to obtain description, from, to
+            } else if (input.startsWith("deadline")) {
+                //Parsing index to obtain description and by
+                int dividerPosition = input.indexOf(" /by ");
+
+                String description = input.substring(9, dividerPosition);
+                String by = input.substring(dividerPosition + 5);
+
+                list[taskCount] = new Deadline(description, by);
+                confirmAddTask(list);
+
+            } else if (input.startsWith("event")) {
+                //index to obtain description, from and to
                 int fromDividerPosition = input.indexOf(" /from ");
                 int toDividerPosition = input.indexOf(" /to ");
 
-                String description = input.substring(6,fromDividerPosition);
-                String from = input.substring(fromDividerPosition+7,toDividerPosition);
-                String to = input.substring(toDividerPosition+5);
+                String description = input.substring(6, fromDividerPosition);
+                String from = input.substring(fromDividerPosition + 7, toDividerPosition);
+                String to = input.substring(toDividerPosition + 5);
 
-                list[taskCount] = new Event(description,from,to);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(list[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have "+ taskCount +" tasks in the list.");
+                list[taskCount] = new Event(description, from, to);
+                confirmAddTask(list);
+            } else {
+                System.out.println("Action unsupported. Try again.");
             }
 
-
-        } while (true);
+        }
 
         System.out.println("Bye Bye! See you soon!");
     }
+
+    private static void confirmMarkDone(Task list) {
+        System.out.println("Nice! I've marked this as done:");
+        System.out.println(list.toString());
+    }
+
+    private static void listTasks(Task[] list) {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + " " + list[i].toString());
+        }
+    }
+
+    private static void confirmAddTask(Task[] list) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(list[taskCount].toString());
+        taskCount++;
+        System.out.println("Now you have " + taskCount + " tasks in the list.");
+    }
 }
 
-
-// brainstorm
-//create todo, deadline, event class from inheritance
-// add if starts with todo, deadline, event,
-// add status icon for type of task
-//create object
