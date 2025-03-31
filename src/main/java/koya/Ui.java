76d18e8133761e6e50f6283a28037e30a143e37a
Jenny Tan <jennytan817@gmail.com>
2025-03-31 22:09:11 +0800

    Add a dashedLine method to improve UI of program
    Cluttered UI makes it hard for the user to read program.

--- src/main/java/koya/Ui.java
+++ src/main/java/koya/Ui.java
@@ -10,29 +10,56 @@ import koya.task.Task;
  * and marked as done.
  */
 public class Ui {
+
+    /**
+     * This method prints a dashed line for clarity purposes.
+     */
+    public static void dashedLine() {
+        System.out.println("\n"+ "------------------------------------------");
+    }
+
+    /**
+     * This method prints a message once a task has been removed.
+     */
     public static void confirmRemoveTask(Task removedTask) {
         System.out.println("Noted! I've removed this task:");
         System.out.println(" " + removedTask.toString());
         System.out.println("You only have " + Koya.taskCount + " tasks in your list");
+        dashedLine();
     }
 
+    /**
+     * This method prints the introduction message to greet and prompt the user to enter a command.
+     */
     public static void printIntroMessage() {
         System.out.println("Hello! I'm Koya");
         System.out.println("What can I do for you?");
+        dashedLine();
     }
 
+    /**
+     * This method prints a message once a task has been mark as done.
+     */
     public static void confirmMarkDone(Task task) {
         System.out.println("Nice! I've marked this as done:");
         System.out.println(task.toString());
+        dashedLine();
     }
 
+    /**
+     * This method prints a message once a task has been added.
+     */
     public static void confirmAddTask(ArrayList<Task> list) {
         System.out.println("Got it. I've added this task:");
         System.out.println(" " + list.get(Koya.taskCount).toString());
         Koya.taskCount++;
         System.out.println("Now you have " + Koya.taskCount + " tasks in the list.");
+        dashedLine();
     }
 
+    /**
+     * This method prints goodbye message once the user decides to exit the program.
+     */
     static void goodbyeMessage() {
         System.out.println("Bye Bye! See you soon!");
     }
